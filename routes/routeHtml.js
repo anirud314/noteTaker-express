4f40aa51a403f6cb6424a22244bd451d6e137482
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-08-18 19:35:16 -0700

    adding routeHtml.js router into the routes folder

---
+++ routes/routeHtml.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const router = require('express').Router(); // getting router fxnality from express lib
+
+/*Creating the routes to the html files  */
+
+router.get('*', (req, res) => { // get all other routs response with index.html file
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/notes', (req, res) => { // get notes.html file
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+module.exports = router;
